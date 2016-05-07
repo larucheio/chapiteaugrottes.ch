52dc587921833de4583ea65756f7ac79a5cf86ff
rspt <julien@rspt.ch>
2016-05-07 19:31:44 +0200

    Compress output css and add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 var gulp        = require('gulp')
 var browserSync = require('browser-sync').create()
 var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
 
 // Static server
 gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./build"
-        }
-    })
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    }
+  })
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("build/*.html").on('change', browserSync.reload);
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("build/*.html").on('change', browserSync.reload);
 })
 
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("build/css"))
-        .pipe(browserSync.stream())
+  return gulp.src("app/scss/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("build/css"))
+    .pipe(browserSync.stream())
 })
 
 
