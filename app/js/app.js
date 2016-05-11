6b0e5e1b254633f0070fbc48322c59c694c27d6b
rspt <julien@rspt.ch>
2016-05-11 15:56:08 +0200

    Random placement for the circle

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,18 @@
-const body = document.body
-body.innerHTML += '<div class="circle"></div>'
+function intRand(min, max) {
+  return Math.floor((Math.random() * max) + min)
+}
+
+const canvas = document.getElementById('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const context = canvas.getContext('2d')
+
+const radius = canvas.width / 10
+const x = intRand(radius, canvas.width - radius * 2)
+const y = intRand(radius, canvas.height - radius * 2)
+
+context.beginPath()
+context.arc(x, y, radius, 0, 2 * Math.PI, false)
+context.fillStyle = 'rgba(63, 100, 175, 0.25)'
+context.fill()
