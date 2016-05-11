a9ca13458dbcbdd647224f0fc0f19f483104fad8
rspt <julien@rspt.ch>
2016-05-11 17:05:15 +0200

    Add a filter for the lineup

--- app/js/app.js
+++ app/js/app.js
@@ -16,3 +16,65 @@ context.beginPath()
 context.arc(x, y, radius, 0, 2 * Math.PI, false)
 context.fillStyle = 'rgba(63, 100, 175, 0.25)'
 context.fill()
+
+
+//
+// Filter //
+//
+const buttonFilterAll = document.querySelectorAll('[data-filter="*"]')[0]
+const buttonFilterDj = document.querySelectorAll('[data-filter="dj"]')[0]
+const buttonFilterLive = document.querySelectorAll('[data-filter="live"]')[0]
+
+buttonFilterAll.onclick = () => {
+  buttonFilterAll.className = 'selected'
+  buttonFilterDj.className = ''
+  buttonFilterLive.className = ''
+
+  // Show the lives
+  const lives = document.querySelectorAll('[data-category="live"]')
+  for (let i = 0; i < lives.length; ++i) {
+    lives[i].style.display = 'table-row'
+  }
+
+  // Show the DJs
+  const djs = document.querySelectorAll('[data-category="dj"]')
+  for (let i = 0; i < djs.length; ++i) {
+    djs[i].style.display = 'table-row'
+  }
+}
+
+buttonFilterDj.onclick = () => {
+  buttonFilterAll.className = ''
+  buttonFilterDj.className = 'selected'
+  buttonFilterLive.className = ''
+
+  // Hide the lives
+  const lives = document.querySelectorAll('[data-category="live"]')
+  for (let i = 0; i < lives.length; ++i) {
+    lives[i].style.display = 'none'
+  }
+
+  // Show the DJs
+  const djs = document.querySelectorAll('[data-category="dj"]')
+  for (let i = 0; i < djs.length; ++i) {
+    djs[i].style.display = 'table-row'
+  }
+}
+
+buttonFilterLive.onclick = () => {
+  buttonFilterAll.className = ''
+  buttonFilterDj.className = ''
+  buttonFilterLive.className = 'selected'
+
+  // Show the lives
+  const lives = document.querySelectorAll('[data-category="live"]')
+  for (let i = 0; i < lives.length; ++i) {
+    lives[i].style.display = 'table-row'
+  }
+
+  // Hide the DJs
+  const djs = document.querySelectorAll('[data-category="dj"]')
+  for (let i = 0; i < djs.length; ++i) {
+    djs[i].style.display = 'none'
+  }
+}
